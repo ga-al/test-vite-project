c145227acfe16d876e85d7889fdc09008e3c0bc2
Lina <77691778+ga-al@users.noreply.github.com>
2025-07-27 16:07:51 +0300

    add base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   server: { port: 3000 },
+  base: '/test-vite-project',
   plugins: [react()],
 })
