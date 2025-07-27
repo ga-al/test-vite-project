621fed555d23c7929e6a70206a716a93746761fe
Lina <77691778+ga-al@users.noreply.github.com>
2025-07-27 16:08:27 +0300

    update base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   server: { port: 3000 },
-  base: '/test-vite-project',
+  base: '/test-vite-project/',
   plugins: [react()],
 })
